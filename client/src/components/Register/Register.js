7b45cd03c6bd3a108fd1f487acb74c9a3e922bde
manuelbiolatiri <manuellaide49@gmail.com>
2020-06-21 22:00:50 +0100

    working nowww

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -28,8 +28,7 @@ class Register extends React.Component {
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         email: this.state.email,
-        password: this.state.password,
-        name: this.state.name
+        password: this.state.password
       })
     })
       .then(response => response.json())
@@ -48,16 +47,6 @@ class Register extends React.Component {
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Register</legend>
-              {/* <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="text"
-                  name="name"
-                  id="name"
-                  onChange={this.onNameChange}
-                />
-              </div> */}
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input

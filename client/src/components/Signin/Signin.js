1d9bd8f25607c8ebd17cb6ed8b1712014a1b79ad
manuelbiolatiri <manuellaide49@gmail.com>
2020-06-21 15:28:06 +0100

    removed pro

--- client/src/components/Signin/Signin.js
+++ client/src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://flashtoken.herokuapp.com/signin', {
+    fetch('/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({

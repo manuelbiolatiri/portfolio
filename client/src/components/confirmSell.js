500882e2f521e6e726e714b24ce686a3783d8910
manuelbiolatiri <manuellaide49@gmail.com>
2020-09-14 10:23:29 +0100

    made the component better

--- client/src/components/confirmSell.js
+++ client/src/components/confirmSell.js
@@ -2,25 +2,19 @@ import React, { useState, useEffect } from 'react';
 import jwtDecode from 'jwt-decode';
 import axios from 'axios';
 import './csell.css';
-import { Link } from 'react-router-dom';
-import Tilt from 'react-tilt';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  Nav,
-  NavItem,
-  NavLink,
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  CardSubtitle,
-  CardBody
-} from 'reactstrap';
-import Converts from './Converter/Converter';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { css } from "@emotion/core";
+import PulseLoader from "react-spinners/PulseLoader";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
+// import Converts from './Converter/Converter';
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: black;
+`;
 
 const ConfirmSell = (props) => {
   const [username, setUsername] = useState('');
@@ -32,6 +26,23 @@ const ConfirmSell = (props) => {
   const [banknumber, setBanknumber] = useState('');
   const [referrals, setReferrals] = useState('');
   const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [photo, setPhoto] = useState('');
+
+  const onChangePhoto = (e) => {
+    setPhoto(URL.createObjectURL(e.target.files[0]))
+    setImage(e.target.files[0])
+  }
+
+  const hideLoader = () => {
+    setLoading(false);
+  }
+
+  const showLoader = () => {
+    setLoading(true);
+  }
   
   let jwt = window.localStorage.getItem('jwt');
   const result = jwtDecode(jwt);
@@ -54,7 +65,7 @@ const ConfirmSell = (props) => {
     //  getRefs
 
     fetch(`http://localhost:3006/api/v1/getrefs/${result.id}`)
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((res) => {
         setReferrals(res.data.rows[0].count);
         console.log(res.data.rows[0].count);
@@ -62,11 +73,13 @@ const ConfirmSell = (props) => {
       .catch((res) => {
         console.log(res);
       });
-  }, [username, referrals]);
+  }, [username, referrals,usd, btc]);
 
 
    const onFormSubmit  = async(e)=>{
     e.preventDefault();
+    showLoader();
+    let customId = "custom-id-yes";
     if (image) {
     const formData = new FormData();
     formData.append('image',image);
@@ -85,40 +98,91 @@ const ConfirmSell = (props) => {
         
     };
    await axios.post("http://localhost:3006/api/v1/auth/sell", formData, config)
-        .then((response) => {
-            alert("The file is successfully uploaded");
-        }).catch((error) => {
+        .then((res) => {
+          console.log(res)
+          console.log(res.status)
+          console.log(res.data)
+          console.log(res.message)
+          
+          if(res.status === 400) {
+            toast.warn(res.data.message, {
+              toastId: customId,
+              position: toast.POSITION.TOP_RIGHT
+            });
+          setErrorMessage(res.data.message);
+          hideLoader();
+          } else if(res.status === 403) {
+            toast.warn(res.data.message, {
+              toastId: customId,
+              position: toast.POSITION.TOP_RIGHT
+            });
+          setErrorMessage(res.data.message);
+          hideLoader();
+          } else if(res.status === 201) {
+            setSuccessMessage(res.data.message);
+          toast.success(res.data.message, {
+            toastId: customId,
+            position: toast.POSITION.TOP_CENTER
+          });
+          setTimeout(() => {
+            
+            window.location.href = "/dashboard"
+            hideLoader();
+          }, 1500)
+          }
+        })
+        .catch((error) => {
           console.log(error)
     });
   } else {
+    setErrorMessage('You need to select a file');
+    toast.warn('You need to select a file', {
+      toastId: customId,
+      position: toast.POSITION.TOP_CENTER
+    });
+    hideLoader();
     console.log('You need to select a file');
 }
 }
   return (
     <div>
       <div className="container center">
-        <div className="flex flex-wrap">
-          <div className="white">
-            <h3>Send Coin to Any of These Wallet Addresses</h3>
+        {errorMessage ? <ToastContainer position= "top-right"
+autoClose= '3000'
+hideProgressBar= {false}
+closeOnClick= {true}
+pauseOnHover= {true}
+draggable= {true}
+progress= {undefined}/> : ''}
+      {successMessage ? <ToastContainer position= "top-right"
+hideProgressBar= {false}
+closeOnClick= {true}
+pauseOnHover= {true}
+draggable= {true}
+progress= {undefined}/> : ''}
+        <div class="griddd">
+          <divs className="container">
+          <div className="br3 ba b--black-10 mv4 w-100   bg-white shadow-5 center">
+          <main className="pa4 black-80">
+                <div className="measure">
+                    <legend className="f1 fw6 ph0 mh0 center">Sell Details</legend>
+            <h2 className="mv2">Send Coin to Any of These Wallet Addresses</h2>
 
-            <h3>wiorivnirvnroihwr48394489y3hfc8n5</h3>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="username">
-                Upload Receipt
-              </label>
-              <input type="file" name="image" onChange={this.onChange} />
+            <p>wiorivnirvnroihwr48394489y3hfc8n5</p>
+            <h2 className="mv2">Amount</h2>
+            <p style={{marginBottom:'2rem'}}>
+              {usd} USD | {btc} BTC
+            </p>
+            {/* <Converts /> */}
             </div>
-            <h3>Amount</h3>
-            <br></br>
-            <h4>
-              {usd} | {btc}
-            </h4>
-          </div>
-          <div>
-            <article className=" bg-white">
+            </main>
+            </div>
+          </divs>
+          <divs className="container">
+            <article className="br3 ba b--black-10 mv4 w-100  bg-white shadow-5 center">
               <main className="pa4 black-80">
                 <div className="measure">
-                  <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                  <fieldset id="confirmsale" className="ba b--transparent ph0 mh0">
                     <legend className="f1 fw6 ph0 mh0">Sell Bitcoin</legend>
                     <div className="mt3">
                       <label className="db fw6 lh-copy f6" htmlFor="bank">
@@ -176,29 +240,33 @@ const ConfirmSell = (props) => {
                         onChange={e => setNote(e.target.value)}
                       />
                     </div>
-                    <div className="button-wrap mt3">
-  <label class ="new-button" for="upload"> Upload Receipt</label>
+                    {/* <div className="button-wrap mt3">
+  <label class ="new-button" for="upload"><span>
+  <FontAwesomeIcon icon={faCloudUploadAlt} style={{color:'white',width:'1rem',height:'1rem'}}/>
+  </span> Upload Receipt</label>
   <input id="upload" type="file" onChange={e => setImage(e.target.files[0])} />
-</div>
-                    {/* <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="image">
-                Upload Receipt
-              </label>
-              <input type="file" name="image" onChange={e => setImage(e.target.files[0])} />
-            </div> */}
+</div> */}
+                    <div className=" button-wrap mt3">
+              
+              <input type="file" name="image" ref={fileInput => this.fileInput = fileInput}
+               style={{display:'none'}} onChange={onChangePhoto} />
+              <button class ="new-button" style={{border:'none'}} onClick={()=> this.fileInput.click()}><span><FontAwesomeIcon icon={faCloudUploadAlt} style={{color:'white',width:'1rem',height:'1rem'}}/>
+  </span> Upload Receipt</button>
+            </div>
+            <div className="mt3">
+            {photo ? <img src={photo} style={{width:'5rem', height:'5rem'}} alt="img"/> : ''}
+            </div>
                   </fieldset>
                   <div className="">
-                    <input
-                      onClick={onFormSubmit}
-                      className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                      type="submit"
-                      value="Sell"
-                    />
+                  <button onClick={onFormSubmit}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib">
+                {loading ? <PulseLoader css={override} color={"black"} size={6}
+        /> : `Confirm Sale`}</button>
                   </div>
                 </div>
               </main>
             </article>
-          </div>
+          </divs>
         </div>
       </div>
     </div>

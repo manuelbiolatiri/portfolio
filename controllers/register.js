709019ac54120210378013d7aa58196f0368fe94
manuelbiolatiri <manuellaide49@gmail.com>
2020-09-18 17:01:16 +0100

    mailgun

--- controllers/register.js
+++ controllers/register.js
@@ -3,15 +3,24 @@ const bcrypt = require('bcryptjs');
 const pool = require('../models/database');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
 
-
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth:{
-        user:'emmanuelbiolatiri49@gmail.com',
-        pass:'biolatiriel123'
+const auth = {
+    auth: {
+      api_key: 'b0f4bf18277d16fe9975fc1ec7ffc40d-7238b007-55ac26b7',
+      domain: 'sandbox25b314747b1d43249c1f7985db8b071f.mailgun.org'
     }
-});
+  }
+   
+  const transporter = nodemailer.createTransport(mg(auth));
+
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth:{
+//         user:'emmanuelbiolatiri49@gmail.com',
+//         pass:'biolatiriel123'
+//     }
+// });
 
 
 const register = {
@@ -31,13 +40,23 @@ console.log(req.body);
 
 const verify = Math.floor((Math.random() * 10000000) + 1);
 
-const mailOption = {
-    from :'emmanuelbiolatiri49@gmail.com', // sender this is your email here
-    to : email, // receiver email2
-    subject: "Account Verification",
-    html: `<h3>Hello Chief, kindly Copy your verification code and paste it on the verification page.<h3><br><hr>
-    <h2>${verify}</h2>`
-}
+// const sendMail = (name, email, subject, text, cb) => {
+    const mailOption = {
+        sender: 'Flashtoken',
+        from: "emmanuelbiolatiri49@gmail.com",
+        to: email,
+        subject: "Account Verification",
+        text: `<h3>Hello Chief, kindly Copy your verification code and paste it on the verification page.<h3><br><hr>
+        <h2>${verify}</h2>`
+    };
+
+// const mailOption = {
+//     from :'emmanuelbiolatiri49@gmail.com', // sender this is your email here
+//     to : email, // receiver email2
+//     subject: "Account Verification",
+//     html: `<h3>Hello Chief, kindly Copy your verification code and paste it on the verification page.<h3><br><hr>
+//     <h2>${verify}</h2>`
+// }
 
 
             // generate bcrypt salt
@@ -127,11 +146,19 @@ const code = Math.random().toString(36).substr(2, 6);
 const amt2receive = 440 * amount_usd;
 
 const mailOption = {
-    from :'emmanuelbiolatiri49@gmail.com', // sender this is your email here
-    to : email, // receiver email2
+    sender: 'Flashtoken',
+    from: "emmanuelbiolatiri49@gmail.com",
+    to: email,
     subject: "Bitcoin Sell Confirmation",
-    html: `<h3>Hello Chief, you have just made a bitcoin sale, kindly wait for admin to verify and be credited.<h3><br><hr>`
-}
+    text: `<h3>Hello Chief, you have just made a bitcoin sale, kindly wait for admin to verify and be credited.<h3><br><hr>`
+};
+
+// const mailOption = {
+//     from :'emmanuelbiolatiri49@gmail.com', // sender this is your email here
+//     to : email, // receiver email2
+//     subject: "Bitcoin Sell Confirmation",
+//     html: `<h3>Hello Chief, you have just made a bitcoin sale, kindly wait for admin to verify and be credited.<h3><br><hr>`
+// }
 
 
         const signUpQuery = `INSERT INTO contracts (transactionId, userId, note,cloudinary_id,image_url, amount_usd,
@@ -146,7 +173,7 @@ const mailOption = {
                 if(error){
                         res.status(400).json({
                             status: 'error',
-                            message: 'mail not sent, please try again'
+                            message: 'An error occured, please try again'
                             
                         });
                 }else{
@@ -160,7 +187,6 @@ const mailOption = {
                         })
                 }
             })
-            console.log(mailerGo());
         } else {
             res.status(403).json({
                 status: 'error',
@@ -264,14 +290,23 @@ console.log(req.body);
 
 const verify = Math.floor((Math.random() * 10000000) + 1);
 
-const mailOption = {
-    from :'emmanuelbiolatiri49@gmail.com', // sender this is your email here
-    to : email, // receiver email2
-    subject: "Account Verification",
-    html: `<h3>Hello Chief, kindly Copy your verification code and paste it on the verification page.<h3><br><hr>
-    <h2>${verify}</h2>`
-}
+// const mailOption = {
+//     from :'emmanuelbiolatiri49@gmail.com', // sender this is your email here
+//     to : email, // receiver email2
+//     subject: "Account Verification",
+//     html: `<h3>Hello Chief, kindly Copy your verification code and paste it on the verification page.<h3><br><hr>
+//     <h2>${verify}</h2>`
+// }
 
+
+const mailOption = {
+    sender: 'Flashtoken',
+    from: "emmanuelbiolatiri49@gmail.com",
+    to: email,
+    subject: "Account Verification",
+    text: `<h3>Hello Chief, kindly Copy your verification code and paste it on the verification page.<h3><br><hr>
+    <h2>${verify}</h2>`
+};
             // generate bcrypt salt
             const salt = await bcrypt.genSalt(10);
             // hash password
